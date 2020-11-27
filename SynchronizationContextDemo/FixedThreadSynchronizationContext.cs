e5ee34d059138ed87668cd84ded23815a745caf9
祝露 <luzhucaine@outlook.com>
2020-11-27 17:06:01 +0800

    SynchronizationContext

---
+++ SynchronizationContextDemo/FixedThreadSynchronizationContext.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynchronizationContextDemo
+{
+    public class FixedThreadSynchronizationContext: SynchronizationContext
+    {
+        private readonly ConcurrentQueue<(SendOrPostCallback Callback, object State)> _workItems;
+
+        public FixedThreadSynchronizationContext()
+        {
+            _workItems = new ConcurrentQueue<(SendOrPostCallback callback, object state)>();
+            var thread = new Thread(StartLoop);
+            Console.WriteLine("FixedThreadSynchronizationContext.ThreadId:{0}", thread.ManagedThreadId);
+            thread.Start();
+
+            void StartLoop()
+            {
+                while (true)
+                {
+                    if (_workItems.TryDequeue(out var workItem))
+                    {
+                        workItem.Callback(workItem.State);
+                    }
+                }
+            }
+        }
+
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            _workItems.Enqueue((d, state));
+        }
+
+        public override void Send(SendOrPostCallback d, object state)
+        {
+            base.Send(d, state);
+        }
+    }
+
+    public class FixedThreadSynchronizationContextTest
+    {
+        public static async Task Test()
+        {
+            var synchronizationContext = new FixedThreadSynchronizationContext();
+            for(int i=0; i< 5; i++)
+            {
+                ThreadPool.QueueUserWorkItem(_ =>
+                {
+                    SynchronizationContext.SetSynchronizationContext(synchronizationContext);
+                    Invoke();
+                });
+            }
+            Console.Read();
+
+            void Invoke()
+            {
+                var dispatchThreadId = Thread.CurrentThread.ManagedThreadId;
+                SendOrPostCallback callback = _ => Console.WriteLine($"Pooled Thread: {dispatchThreadId}; Execution Thread: {Thread.CurrentThread.ManagedThreadId}");
+                SynchronizationContext.Current.Post(callback, null);
+            }
+        }         
+    }
+
+    public class FixedThreadSynchronizationContextAwaitTest
+    {
+        public static async Task Test()
+        {
+            SynchronizationContext.SetSynchronizationContext(new FixedThreadSynchronizationContext());
+            //await Task.Delay(100)
+            await Task.Delay(100).ConfigureAwait(false);//将不在同步上下文中执行
+            Console.WriteLine("Await Thread:{0}", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
---
+++ SynchronizationContextDemo/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SynchronizationContextDemo
+{
+    public delegate void Delegate1();
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ThreadPool.QueueUserWorkItem(_ => Text = "Hello World!");//直接调用抛出跨线程异常
+            //ThreadPool.QueueUserWorkItem(SetText, "abc");//work
+
+            //以下写法不work，即在委托中访问同步上下文的方式会取到null
+            //ThreadPool.QueueUserWorkItem(_ => SynchronizationContext.Current.Post(a => Text = "Hello World!" + a, "abc"));
+            var context = SynchronizationContext.Current;
+            ThreadPool.QueueUserWorkItem(_ => context.Post(a => Text = "Hello World!" + a, "abc"));//work
+
+            //new Thread(new ParameterizedThreadStart(SetText)).Start("test");//work
+        }
+
+        private void SetText(object text)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Delegate1(() => this.SetText(text)));//在委托中再次调用此方法，则进入到下面的else
+            }
+            else
+            {
+                this.Text = "Hello World!" + text;
+            }
+        }
+    }
+}
